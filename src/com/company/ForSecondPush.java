cbb30415a726b8702db9723681f3525398dac5c4
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 16:35:19 +0400

    I

--- src/com/company/ForSecondPush.java
+++ src/com/company/ForSecondPush.java
@@ -4,6 +4,7 @@ public class ForSecondPush {
     public static void toPrint(){
         System.out.println("I am Pushed on second turn");
         System.out.println("I am come later");
-        System.out.println("it is 4th");
+        System.out.println("It is 4th");
+        System.out.println("It is 5th");
     }
 }
