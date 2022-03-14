93385805a1c9aa5fc6899a0f417c6c92714a9db7
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 15:11:45 +0400

    Initial commit it is second test from JDK to GitHub

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello world");
+        A.pp();
+        TestFirst.printIt();
+
+    }
+}
+class A{
+    public static void pp(){
+        System.out.println("Hello !!!!!!!!!!!");
+    }
+}
---
+++ src/com/company/TestFirst.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TestFirst {
+    static void printIt(){
+
+        System.out.println("Hello I am from TestFirst");
+    }
+}
