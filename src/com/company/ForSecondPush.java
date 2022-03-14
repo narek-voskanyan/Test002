63d5b496b99bd6986c4170098d3ae2ac57854d37
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 15:49:11 +0400

    inite

--- src/com/company/ForSecondPush.java
+++ src/com/company/ForSecondPush.java
@@ -3,5 +3,6 @@ package com.company;
 public class ForSecondPush {
     public static void toPrint(){
         System.out.println("I am Pushed on second turn");
+        System.out.println("I am come later");
     }
 }
